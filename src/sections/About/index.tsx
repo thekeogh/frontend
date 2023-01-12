710823cf8ea788191b50667d163b1b13aa585149
Steve McKeogh <steve.mckeogh@gmail.com>
2023-01-12 09:05:38 +0000

    fix(about): Small change to text (added space before 'Check')

--- src/sections/About/index.tsx
+++ src/sections/About/index.tsx
@@ -27,7 +27,7 @@ export function About(props: Props) {
             <Avatar />
             <blockquote>
               <p>I'm Steve McKeogh, a highly experienced Full Stack Staff Software Engineer with over twenty years experience in the industry.</p>
-              <p>I currently specialise in Node.js, TypeScript, PostgreSQL, GraphQL and React but have many more strings to my bow.Check out <a href="#skills" onClick={() => scroller.scrollTo("skills", { smooth: true })}>my skills</a> if you want to know more.</p>
+              <p>I currently specialise in Node.js, TypeScript, PostgreSQL, GraphQL and React but have many more strings to my bow. Check out <a href="#skills" onClick={() => scroller.scrollTo("skills", { smooth: true })}>my skills</a> if you want to know more.</p>
               <p>Starting way back in 1999 as a junior web developer and moving up to a full stack staff software engineer.</p>
               <p>I'm currently based out of my little hometown of London, England, I also spent four happy years in Asia when I relocated to Bangkok, Thailand back in 2018.</p>
             </blockquote>
